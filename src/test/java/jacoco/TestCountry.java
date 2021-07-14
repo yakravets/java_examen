7906b314ff486876523990238e87941e75bb4659
Yaroslav <yakravets@ktc.rovno.ua>
2021-07-14 16:19:39 +0300

    update test file

--- src/test/java/jacoco/TestCountry.java
+++ src/test/java/jacoco/TestCountry.java
@@ -6,21 +6,15 @@ import org.junit.Test;
 
 public class TestCountry {
 
-    private Country country;
-
-    public TestCountry() {
-
-        this.country = new Country("Ukraine", "UA");
-
-    }
-
     @Test
     public void CheckCountryName() {
+        Country country = new Country("Ukraine", "UA");
         Assert.assertTrue(country.getName().equals("Ukraine"));
     }
 
     @Test
     public void CheckCountryShortName(){
+        Country country = new Country("Ukraine", "UA");
         Assert.assertEquals("Short name asserted", country.getShort_name(), "UA");
     }
 }
