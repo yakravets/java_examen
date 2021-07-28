5d099a2d767c988ff235094718ac252c86dd0a6d
yakravets <30793298+yakravets@users.noreply.github.com>
2021-07-29 02:08:05 +0300

    update test

--- frontend/src/components/app/About/About.test.js
+++ frontend/src/components/app/About/About.test.js
@@ -1,10 +1,9 @@
 import React from "react";
 
-import { configure } from "enzyme";
+import { configure, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 
-import { shallow, mount } from "enzyme";
 import About from './About';
 import toJson from "enzyme-to-json";
 
